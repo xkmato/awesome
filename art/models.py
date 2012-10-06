8fd871ce8e2dd658730b6d812b62c78eb08ccdd6
kenneth <kenneth@kudu.ug>
2012-10-06 19:33:02 +0300

    Uploaded the edited temp

--- art/models.py
+++ art/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 ART_CHOICES = ((u'Film',u'Film'),(u'Music',u'Music'),(u'FineArt',u'FineArt'),
-               (u'Sculpture',u'Sculpture'),(u'Photography',u'Photography'),(u'TV',u'TV'))
+               (u'Craft',u'Craft'),(u'Photography',u'Photography'),(u'TV',u'TV'),(u'Literature',u'Literature'))
 class ArtPiece(models.Model):
     owners = models.ManyToManyField(User)
     art_type = models.CharField(max_length=50,choices=ART_CHOICES)
@@ -23,6 +23,13 @@ class ArtPiece(models.Model):
     def get_likes(self):
         return self.like_set.all()
 
+    def get_extra_attributes(self):
+        for attribute in list(self.extraattribute_set.all()):
+            if attribute.user_involved:
+                yield (attribute.attribute,attribute.user_involved)
+            elif attribute.other_details:
+                yield (attribute.attribute,attribute.other_details)
+
 class Credit(models.Model):
     name = models.CharField(max_length=100)
     register_user = models.ForeignKey(User)
@@ -40,8 +47,21 @@ class Like(models.Model):
     def __unicode__(self):
         return self.liker.username
 
-#class ExtraAttributes(models.Model):
-#    pass
+class ExtraAttribute(models.Model):
+    art = models.ForeignKey(ArtPiece)
+    attribute = models.CharField(max_length=100)
+    user_involved = models.ForeignKey(User,null=True,help_text="this is only applicable if the attribute is about a user")
+    other_details = models.TextField(blank=True)
+
+    def clean(self):
+        if self.user_involved and self.other_details:
+            raise ValidationError('An Extra attribute can\'t have both user and other details set')
+
+        if not (self.user_involved and self.other_details):
+            raise ValidationError('One of other details or user involved must be set for an attribute')
+
+    def __unicode__(self):
+        return self.art.name
 
 class Comment(models.Model):
     comment = models.TextField()
@@ -54,6 +74,8 @@ class Comment(models.Model):
         if self.commented_on_art and self.commented_on_gallery:
             raise ValidationError('A comment can either be for an art Piece or Gallery but not both')
 
+        elif not(self.commented_on_art and self.commented_on_gallery):
+            raise ValidationError('What is this comment for?')
 
 class Gallery(models.Model):
     owners = models.ManyToManyField(User)
